522675472b5c46e5f73bbd3a7bac4e6a148bb4ec
hung <ff789456741@gmail.com>
2025-08-05 16:11:40 +0800

    style(index): 新增側邊欄及頁首

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,145 +1,168 @@
 <script setup>
-  import { ref } from 'vue';
-  import { Menu as IconMenu, Message, Setting } from '@element-plus/icons-vue';
+  // import { useAuthStore } from '@/stores/auth';
+  import { useRoute } from 'vue-router';
+  // const authStore = useAuthStore();
+  import logo from '@/assets/images/logo.svg';
 
-  const item = {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  };
-  const tableData = ref(Array.from({ length: 20 }).fill(item));
+  const route = useRoute();
+
+  const menuItems = [
+    { index: '/', title: '後台人員管理' },
+    { index: '/test1', title: '會員資料查詢' },
+    { index: '/test2', title: '食譜管理' },
+    { index: '/test3', title: '食材學堂管理' },
+    { index: '/test4', title: '商品管理' },
+    { index: '/test5', title: '訂單查詢' },
+    { index: '/test6', title: '留言檢舉管理' },
+    { index: '/test7', title: '通知管理' },
+  ];
 </script>
 
 <template>
-  <el-container
-    class="layout-container-demo"
-    style="height: 500px"
-  >
-    <el-aside width="200px">
-      <el-scrollbar>
-        <el-menu :default-openeds="['1', '3']">
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><message /></el-icon>
-              Navigator One
-            </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="1-1">Option 1</el-menu-item>
-              <el-menu-item index="1-2">Option 2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="1-3">Option 3</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title>Option4</template>
-              <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-sub-menu index="2">
-            <template #title>
-              <el-icon><icon-menu /></el-icon>
-              Navigator Two
-            </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="2-1">Option 1</el-menu-item>
-              <el-menu-item index="2-2">Option 2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="2-3">Option 3</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="2-4">
-              <template #title>Option 4</template>
-              <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-sub-menu index="3">
-            <template #title>
-              <el-icon><setting /></el-icon>
-              Navigator Three
-            </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="3-1">Option 1</el-menu-item>
-              <el-menu-item index="3-2">Option 2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="3-3">Option 3</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="3-4">
-              <template #title>Option 4</template>
-              <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-        </el-menu>
-      </el-scrollbar>
+  <el-container class="layout-container">
+    <el-aside>
+      <router-link
+        to="/"
+        class="logo__link"
+      >
+        <div class="logo__container">
+          <div class="logo__pic">
+            <img
+              :src="logo"
+              alt=""
+              class="logo__img"
+            />
+          </div>
+          <h2 class="logo__title">後台管理系統</h2>
+        </div>
+      </router-link>
+      <el-menu
+        :default-active="route.path"
+        router
+      >
+        <template
+          v-for="item in menuItems"
+          :key="item.index || item.id"
+        >
+          <el-menu-item :index="item.index">
+            <span class="el-menu-title">{{ item.title }}</span>
+          </el-menu-item>
+        </template>
+      </el-menu>
     </el-aside>
 
     <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <div class="toolbar">
-          <el-dropdown>
-            <el-icon style="margin-right: 8px; margin-top: 1px">
-              <setting />
-            </el-icon>
-            <template #dropdown>
-              <el-dropdown-menu>
-                <el-dropdown-item>View</el-dropdown-item>
-                <el-dropdown-item>Add</el-dropdown-item>
-                <el-dropdown-item>Delete</el-dropdown-item>
-              </el-dropdown-menu>
-            </template>
-          </el-dropdown>
-          <span>Tom</span>
+      <el-header>
+        <div class="header">
+          <div class="header-user">黃維尼</div>
+          <button class="header-logout">登出</button>
         </div>
       </el-header>
 
       <el-main>
-        <el-scrollbar>
-          <el-table :data="tableData">
-            <el-table-column
-              prop="date"
-              label="Date"
-              width="140"
-            />
-            <el-table-column
-              prop="name"
-              label="Name"
-              width="120"
-            />
-            <el-table-column
-              prop="address"
-              label="Address"
-            />
-          </el-table>
-        </el-scrollbar>
+        <RouterView />
       </el-main>
     </el-container>
   </el-container>
 </template>
 
 <style lang="scss" scoped>
-  .layout-container-demo .el-header {
-    position: relative;
-    background-color: var(--el-color-primary-light-7);
-    color: var(--el-text-color-primary);
+  .layout-container {
+    height: 100vh;
   }
-  .layout-container-demo .el-aside {
-    color: var(--el-text-color-primary);
-    background: color(backgroundColor, recipe);
+
+  .el {
+    &-aside {
+      width: px(300);
+      padding: px(26) 0;
+      background-color: color(backgroundColor, recipe);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    &-menu {
+      width: fit-content;
+      background-color: transparent;
+      border-right: none;
+
+      &-item {
+        @include fontSet($size: px(24), $lh: 1.2, $color: color(text, light), $ls: 0.1em);
+        border-radius: px(20);
+        justify-content: center;
+        transition:
+          background-color 0.3s ease-in-out,
+          color 0.3s ease-in-out;
+
+        &.is-active,
+        &:hover {
+          color: color(text, dark);
+          background-color: color(backgroundColor, beige);
+        }
+
+        & + & {
+          margin-top: 20px;
+        }
+      }
+    }
+    &-header {
+      background-color: color(backgroundColor, beige);
+      @include headerBoxShadow;
+      z-index: 1;
+      height: auto;
+
+      .header {
+        @include flex($jc: flex-end);
+        column-gap: px(30);
+        margin-left: auto;
+        padding: px(15) px(50);
+
+        &-user {
+          @include fontSet($size: px(20), $lh: 1.2, $color: color(text, dark), $ls: 0.1em);
+        }
+
+        &-logout {
+          @include fontSet($size: px(18), $lh: 1.2, $color: color(button, main), $ls: 0.1em);
+
+          background-color: color(backgroundColor, beige);
+          padding: px(10) px(20);
+          border: px(1) solid color(backgroundColor, recipe);
+          border-radius: px(15);
+        }
+      }
+    }
+
+    &-main {
+      overflow: auto;
+      background-color: color(backgroundColor, beige);
+      padding: px(20) px(30);
+    }
   }
-  .layout-container-demo .el-menu {
-    border-right: none;
-  }
-  .layout-container-demo .el-main {
-    padding: 0;
-  }
-  .layout-container-demo .toolbar {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-    right: 20px;
+
+  .logo {
+    &__link {
+      text-decoration: none;
+    }
+
+    &__container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      row-gap: px(15);
+      margin-bottom: px(90);
+    }
+
+    &__pic {
+      width: 100px;
+    }
+
+    &__img {
+      @include fit();
+      aspect-ratio: 48 / 32;
+    }
+
+    &__title {
+      @include fontSet($size: px(30), $lh: 1.2, $color: color(text, light), $ls: 0.1em);
+    }
   }
 </style>
