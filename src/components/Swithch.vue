fa61b92ea10878aed966e439d37f7af6964b41fe
Lau-ra-177 <kwbby8@gmail.com>
2025-08-05 18:09:41 +0800

    feat(Switch, Table) 新增兩個元件

---
+++ src/components/Swithch.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { ref } from 'vue';
+  const value5 = ref(true);
+
+    const props = defineProps({
+    yes: {
+      type: String,
+      default: '正常',
+    },
+    no: {
+      type: String,
+      default: '停權',
+    },
+  });
+</script>
+
+<template>
+  <el-switch
+    v-model="value5"
+    class="ml-2 custom-switch"
+    inline-prompt
+    style="--el-switch-on-color: #f4a259; --el-switch-off-color: #828282;"
+    :active-text="yes"
+    :inactive-text="no"
+    size="large"
+    width="70px"
+  />
+</template>
+
+<style lang="scss" scoped>
+</style>
