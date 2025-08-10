1f38eed47d3ce83ea243b1cf4156a935b7c2603e
Lau-ra-177 <kwbby8@gmail.com>
2025-08-10 19:17:04 +0800

    fix(Table)新增後台會使用到的icon，可以直接更改type使用

--- src/constants/order.js
+++ src/constants/order.js
@@ -12,5 +12,5 @@ export const columns = [
   { prop: 'date', label: '訂單日期' },
   { prop: 'price', label: '金額' },
   { prop: 'status', label: '訂單狀態' },
-  { prop: 'icon', label: '內容', type: 'button', width: 60 },
+  { prop: 'icon', label: '內容', type: 'button-detail', width: 60 },
 ];
