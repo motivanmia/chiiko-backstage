f7cf050298111c4a32f8a448251b0cba37726bc4
motivanmia <waniowo@gmail.com>
2025-08-19 00:57:46 +0800

    fix(recipe):調整版面&新增篩選邏輯

--- src/pages/Recipe.vue
+++ src/pages/Recipe.vue
@@ -1,50 +1,20 @@
 <script setup>
-  import 'element-plus/theme-chalk/el-cascader.css';
-
   import { ref } from 'vue';
-  // import { Search } from '@element-plus/icons-vue';
-  import DropMenu from '@/components/common/DropMenu.vue';
-
-  import Search from '@/components/common/Search.vue';
-  import MyTitle from '@/components/common/Title.vue';
-  import IncreaseButton from '@/components/common/IncreaseButton.vue';
+  import { useFilter } from '@/composables/useFilter';
   import Table from '@/components/Table.vue';
+  import TheHeader from '@/components/common/TheHeader.vue';
 
-  const selectedStatus = ref(''); // 用於儲存使用者選擇的結果
-
-  const recipeOptions = [
-    {
-      value: 'draft',
-      label: '草稿',
-    },
-    {
-      value: 'approved',
-      label: '已審核',
-    },
-    {
-      value: 'pending',
-      label: '待審核',
-    },
-    {
-      value: 'remove',
-      label: '已下架',
-    },
-  ];
-
-  // 搜尋
+  const searchOption = ref([]);
   const searchText = ref('');
 
-  const handleSearch = () => {};
-
-  //內容
-
+  // 內容
   const columns = ref([
     { prop: 'number', label: '食譜編號', width: 200 },
     { prop: 'category', label: '食譜分類', width: 150 },
     { prop: 'name', label: '食譜名稱' },
     { prop: 'author', label: '作者' },
-    { prop: 'date', label: '新增日期', width: 200 },
-    { prop: 'status', label: '會員狀態', type: 'status', width: 200 },
+    { prop: 'date', label: '新增日期', width: 150 },
+    { prop: 'status', label: '狀態', width: 200 },
     { prop: 'icon', label: '詳細', type: 'button-detail', width: 100 },
     { prop: 'del', label: '刪除', type: 'button-del', width: 100 },
   ]);
@@ -57,8 +27,6 @@
       author: '管理員',
       date: '2025-07-07',
       status: '草稿',
-      icon: '',
-      del: '',
     },
     {
       number: 'R01',
@@ -67,8 +35,6 @@
       author: '塔馬可吉',
       date: '2025-07-07',
       status: '待審核',
-      icon: '',
-      del: '',
     },
     {
       number: 'R01',
@@ -77,86 +43,70 @@
       author: '管理員',
       date: '2025-07-07',
       status: '已發布',
-      icon: '',
-      del: '',
     },
   ]);
+
+  // 定義選項生成邏輯
+  const optionsGenerator = (data) => {
+    // 定義主選項
+    const mainOptions = [
+      { label: '管理員', value: '管理員' },
+      { label: '會員', value: ['not', '管理員'] },
+    ];
+
+    // 為每個主選項生成子選項
+    const secOptions = mainOptions.map((mainOpt) => {
+      let subChildren = [];
+
+      if (mainOpt.value === '管理員') {
+        // 主選項是admin時 篩選出author為管理員的資料
+        const adminData = data.filter((item) => item.author === '管理員');
+        // 依據資料項目生成子選項(狀態)
+        const uniqueName = [...new Set(adminData.map((item) => item.status))];
+        subChildren = uniqueName.map((status) => ({
+          label: status,
+          value: status,
+        }));
+      } else if (mainOpt.value !== '管理員') {
+        // 主選項是non-admin時 篩選出author不是管理員的資料
+        const nonAdminData = data.filter((item) => item.author !== '管理員');
+        // 依據資料項目生成子選項(狀態)
+        const uniqueName = [...new Set(nonAdminData.map((item) => item.status))];
+        subChildren = uniqueName.map((status) => ({
+          label: status,
+          value: status,
+        }));
+      }
+      return {
+        ...mainOpt,
+        children: subChildren,
+      };
+    });
+    return secOptions;
+  };
+
+  // 使用composable生成選項
+  const { dropOptions, filterData } = useFilter(
+    tableData,
+    searchOption,
+    searchText,
+    optionsGenerator,
+    'author',
+    'status',
+  );
 </script>
 
 <template>
-  <div class="recipe-board">
-    <div class="recipe-board__manage">
-      <MyTitle title="食譜管理"></MyTitle>
-      <IncreaseButton></IncreaseButton>
-    </div>
-
-    <div class="recipe-board__query">
-      <DropMenu
-        v-model="selectedStatus"
-        :options="recipeOptions"
-        class="recipe-board__drop"
-      ></DropMenu>
-      <Search class="recipe-board__search" />
-    </div>
-
-    <div class="recipe-board__contents">
-      <Table
-        :table-data="tableData"
-        :columns="columns"
-      >
-        <template #del>
-          <Icon
-            class="del-button"
-            icon-name="del"
-          />
-        </template>
-      </Table>
-    </div>
-  </div>
+  <TheHeader
+    title="食譜管理"
+    v-model:searchOption="searchOption"
+    v-model:searchText="searchText"
+    :dropOptions="dropOptions"
+  />
+  <Table
+    :table-data="filterData"
+    :columns="columns"
+  />
 </template>
 
-<style lang="scss" scoped>
-  .recipe-board {
-    &__manage {
-      display: flex;
-      justify-content: space-between;
-      align-content: center;
-    }
-
-    &__query {
-      display: grid;
-      grid-template-columns: repeat(5, 1fr);
-      grid-template-rows: auto;
-      margin-top: 27px;
-      position: relative;
-
-      &::after {
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 1px;
-        background-color: color(backgroundColor, recipe);
-        bottom: -30px;
-      }
-
-      .recipe-board__drop {
-        grid-column: 2/3;
-        grid-row: 1;
-        align-self: center;
-      }
-      .recipe-board__search {
-        grid-column: 5/6;
-        grid-row: 1;
-      }
-    }
-
-    .recipe-board__contents {
-      margin-top: 50px;
-
-      .del-button {
-        font-size: 20px;
-        cursor: pointer;
-      }
-    }
-  }
-</style>
+<style lang="scss" scoped></style>
