0940290317435729153d30486efe75d8710bae5c
Grey-Liu <sugarfree5807@gmail.com>
2025-08-21 17:41:33 +0800

    feat(componets):新增圖片提示彈窗/feat(school):食材詳細頁面引入圖片彈窗

---
+++ src/components/common/ImageTip.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import Icon from '@/components/common/Icon.vue';
+</script>
+
+<template>
+  <el-tooltip
+    placement="right"
+    effect="light"
+    :enterable="true"
+    :show-after="80"
+  >
+    <template #content>
+      <img
+        class="tip-img"
+        src="/src/assets/image/tip.png"
+        alt="預覽圖"
+      />
+    </template>
+
+    <!-- 觸發的 icon（你的自訂 Icon 元件） -->
+    <Icon
+      icon-name="edit"
+      class="icon"
+      @click="$emit('edit-click', scope.row)"
+    />
+  </el-tooltip>
+</template>
+
+<style lang="scss" scoped>
+  .tip-img {
+    width: 700px;
+    height: auto;
+    display: block;
+  }
+  .hover-icon {
+    cursor: help;
+  }
+</style>
